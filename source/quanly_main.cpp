8fd62bb905a46d65c09df66167bf2e4d9dd82f09
DuyNT <ntd121996@gmail.com>
2020-03-12 00:41:01 +0700

    backtracking

--- source/quanly_main.cpp
+++ source/quanly_main.cpp
@@ -1,12 +1,58 @@
 #include "quanly.h"
 
+int count = 1;
+bool first = true;
+int n = 9;
+char Array[9];
+int Check[9] ={ 1,1,1,1,1,1,1,1,1};
+int value;
+bool CheckValid( int i, int k)
+{
+    if( k == 8 )
+    {
+        if( first )
+        {
+            first = false;
+            return false;
+        }
+    }
+    if (count == 20) return false;
+    return true;
+}
+void printfSolution()
+{
+    cout << "So thu " << count++ << ":";
+    value = atoi( Array );
+    cout << value << endl;
 
+}
+void Try( int k )
+{
+    int i;
+    for( i = 0x30; i <= 0x39; i++ )
+    {
+        if( CheckValid( i , k ))
+        {
+            Array[ k ] = i;
+            
+            if (k  == n - 1)
+            {
+                printfSolution();
+            }
+            else
+            {
+                Try ( k + 1 );
+            }
+            
+        }
+    }
 
+}
 
 int main ( int argc, char** argv )
 {
 
-
+    Try( 0 );
 
     return 0;
 }
